9dc51548210680a52dafb21f7372c78c1623b14f
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2021-12-09 22:40:02 +0100

    + Let It Snow project

--- src/data/data.js
+++ src/data/data.js
@@ -3,12 +3,12 @@ const data = {
     links: ["https://github.com/vadimgierko", "https://pl.linkedin.com/in/vadimgierko"],
     projects: [
         {
-            name: "Blogging Platform",
+            name: "Blogging Platform with Markdown Text Editor",
             img: "img/blogging-platform-vadim-gierko-screen.png",
             gp: "https://vadimgierko.github.io/blogging-platform/",
             repoUrl: "https://github.com/vadimgierko/blogging-platform",
             language: "React, React-Context, React-Router, React-Markdown, Bootstrap, Bootswatch, Firebase (realtime database, security rules & authentication), GitHub Pages",
-            description: "Blogging Platform allows you to create & run your blog (or many blogs) for free after creating a free user account, write & edit your articles with simple & intuitive markdown text editor (which was also created by myself) & read published blogs without authentication. This is my newest, most complex & advanced responsive full-stack single-page real-world application, which supports all kinds of CRUD features, in which I have used all of my previous knowledge and skills in the field of front-end development (React, Bootstrap) and realtime database integration (Firebase).",
+            description: "This full-stack SPA allows you to create & run your blog (or many blogs) for free after creating a free user account, write & edit your articles with simple & intuitive markdown text editor (which was also created by myself) & read published blogs without authentication. This is my newest, most complex & advanced responsive full-stack single-page real-world application, which supports all kinds of CRUD features, in which I have used all of my previous knowledge and skills in the field of front-end development (React, Bootstrap) and realtime database integration (Firebase).",
         },
         {
             name: "GitHub-API-Users-Search-App",
@@ -27,12 +27,20 @@ const data = {
             description: "Mind Maps App is a very simple, intuitive and easy to use application for online mindmapping. With this app you can create & edit a mind map with keywords in any place around the core keyword/s (you can build really impressive multi-level structure of nested keywords) You sign up & create the account, so you will be able to save a mind map as a project (for future edition) in your user's database (Firebase) and/or download your mind map as a PNG file with one click!"
         },
         {
-            name: "Pixel Draw App",
-            img: "img/pixel-draw-app-screen-vadim-gierko.png",
-            gp: "https://vadimgierko.github.io/PixelDrawApp/",
-            repoUrl: "https://github.com/vadimgierko/PixelDrawApp",
-            language: "React, Bootstrap",
-            description: "Pixel Drawing App allows you to create pictures by filling pixels with color. All you need is to pick the color from color picker and then click on any pixel on the board to fill it with the chosen color! You can save a pixel picture as a project in local storage (your browser), so you can work on that later and it will be possible to download the image soon (when I rewrite it with p5.js). Also the app will be integrated with Firebase, like MindMapsApp, so there will be the possibility to sign up and save/ edit your pixel drawings in database."
+            name: "Old School 2D Racing Game",
+            img: "img/old-school-racing-game-p5-js-vadim-gierko-2.png",
+            gp: "https://vadimgierko.github.io/old-school-2d-racing-game/",
+            repoUrl: "https://github.com/vadimgierko/old-school-2d-racing-game",
+            language: "JavaScript, p5.js library, Bootstrap Icons",
+            description: "Old-school 2D racing game created in JavaScript using p5.js library. I've started to develop this game at the beggining of my programming journey, but... had not enough knowledge to finish it then. So now, after 10 months brake I've figured all bugs & problems out, so the game is finally completed! I love working with p5.js, which alows you to create animations like this in that game, and thanks to this library I've made my first steps in programming learning with Khan Academy online courses, so it was a pleasure to come back to old habbits after 10 months of working mainly with React & other technologies ;-) Car's design was inspired by drawing of my son! Enjoy!"
+        },
+        {
+            name: "Let It Snow! Falling Snow Animation",
+            img: "img/let-it-snow-p5-javascript-animation-vadim-gierko.png",
+            gp: "https://vadimgierko.github.io/let-it-snow/",
+            repoUrl: "https://github.com/vadimgierko/let-it-snow",
+            language: "JavaScript, p5.js library",
+            description: "This Christmas Online Animated Musical Card was made using Javascript & p5.js library. To make the animation seem more realistic, I created a pseudo 3D illusion thanks to basic knowledge of perspective: 1). there are 3 layers of snow, 2). more distant snowflakes are smaller, while closer are larger, 3). more distant snowflakes fall slower, and closer ones fall faster. Let it snow!"
         },
         {
             name: "Live Markdown Text Editor",
@@ -43,12 +51,12 @@ const data = {
             description: "Live Markdown Text Editor lets you write text using markdown language & render it as an HTML. The feature can be (and will be) used as a text editor in my Blogging Platform App (my most advanced fullstack application which comes very soon) & many others apps."
         },
         {
-            name: "Old School 2D Racing Game",
-            img: "img/old-school-racing-game-p5-js-vadim-gierko-2.png",
-            gp: "https://vadimgierko.github.io/old-school-2d-racing-game/",
-            repoUrl: "https://github.com/vadimgierko/old-school-2d-racing-game",
-            language: "JavaScript, p5.js library, Bootstrap Icons",
-            description: "Old-school 2D racing game created in JavaScript using p5.js library. I've started to develop this game at the beggining of my programming journey, but... had not enough knowledge to finish it then. So now, after 10 months brake I've figured all bugs & problems out, so the game is finally completed! I love working with p5.js, which alows you to create animations like this in that game, and thanks to this library I've made my first steps in programming learning with Khan Academy online courses, so it was a pleasure to come back to old habbits after 10 months of working mainly with React & other technologies ;-) Car's design was inspired by drawing of my son! Enjoy!"
+            name: "Pixel Draw App",
+            img: "img/pixel-draw-app-screen-vadim-gierko.png",
+            gp: "https://vadimgierko.github.io/PixelDrawApp/",
+            repoUrl: "https://github.com/vadimgierko/PixelDrawApp",
+            language: "React, Bootstrap",
+            description: "Pixel Drawing App allows you to create pictures by filling pixels with color. All you need is to pick the color from color picker and then click on any pixel on the board to fill it with the chosen color! You can save a pixel picture as a project in local storage (your browser), so you can work on that later and it will be possible to download the image soon (when I rewrite it with p5.js). Also the app will be integrated with Firebase, like MindMapsApp, so there will be the possibility to sign up and save/ edit your pixel drawings in database."
         },
         {
             name: "Quiz about Lublin",
@@ -57,7 +65,7 @@ const data = {
             repoUrl: "https://github.com/vadimgierko/Quiz-about-Lublin",
             language: "React, React-Bootstrap",
             description: "This is the quiz about my hometown - Lublin - made using React and React-Bootstrap. Answer to the 12 questions and check out your knowledge about Lublin!"
-        }
+        },
     ],
     page: `https://vadimgierko.com/`,
 };
