e13b58255d440190e867e5498ee2095d5c895ba1
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2022-02-20 23:52:30 +0100

    about section updates

--- src/components/About.js
+++ src/components/About.js
@@ -1,41 +1,79 @@
 export default function About() {
-    return (
-        <div className="col-md text-justify" id="about" style={{paddingTop: "70px"}}>
-            <h3>Hello, World!</h3>
-            <p>I'm a <strong>self-taught JavaScript / React developer</strong> with 1 year of non-commercial programming experience & artistic, blogging and musical background.</p>
-            <h5>What can I build:</h5>
-            <p>I can build <strong>responsive website or full-stack single page application</strong> that has:</p>
-            <ul>
-                <li>all kinds of CRUD features (create, read, update & delete any kind of content) integrated with Firebase</li>
-                <li>dynamic & nested routing (React Router)</li>
-                <li>forms, search / filter engines</li>
-                <li>tagged content & searching by tags mechanism</li>
-                <li>users authentication (Firebase 9.1)</li>
-                <li>visualizations & animations (p5.js)</li>
-                <li>API connection</li>
-                <li>simple customized CMS written by me</li>
-                <li>dark mode</li>
-                <li>Bootstrap 5+ & CSS3 (+ Bootswatch & Bootstrap Icons) styling</li>
-            </ul>
-            <h5>What technologies I usually use:</h5>
-            <ul>
-                <li><strong>JavaScript</strong> (ES6)</li>
-                <li><strong>React</strong> 17</li>
-                <li><strong>React Router</strong> 5+</li>
-                <li><strong>Firebase</strong> 9</li>
-                <li><strong>Bootstrap</strong> 5.1</li>
-                <li><strong>HTML5 / CSS3</strong></li>
-                <li><strong>p5.js</strong> (for visualisations or animations)</li>
-                <li><strong>jQuery</strong></li>
-                <li>Atomic Design Pattern</li>
-            </ul>
-            <h5>What else I am interested in:</h5>
-            <ul>
-                <li>visual thinking (sketchnoting and mindmapping)</li>
-                <li>creative process management (I wrote an ebook & ran blog on the topic)</li>
-                <li>composing music (using GuitarPro 7.5)</li>
-                <li>self-development</li>
-            </ul>
-        </div>
-    );
+	return (
+		<div
+			className="col-md text-justify"
+			id="about"
+			style={{ paddingTop: "70px" }}
+		>
+			<h1>Hello, World!</h1>
+			<p>
+				My name is Vadim Gierko and I’m a{" "}
+				<strong>self-taught React / Firebase developer</strong> with
+				more than a year of non-commercial programming experience &...
+				an artistic, blogging and musical background.
+			</p>
+			<h2>What I love to develop</h2>
+			<p>
+				I'm insanely excited to build complex <strong>SPA</strong>s with{" "}
+				<strong>React & Firebase</strong> (that's why I called myself a
+				React/Firebase developer above 😆) with users authentication &
+				all of CRUD features.
+			</p>
+			<p>
+				I'm not a back-end developer, but I hope to become a full-stack
+				in the future & using Firebase as BaaS gives me the opportunity
+				to feel like one 😆 and build in fact full-stack apps.
+			</p>
+			<h2>Technologies, languages, tools & frameworks I use</h2>
+			<ul>
+				<li>
+					<strong>JavaScript</strong> (ES6)
+				</li>
+				<li>
+					<strong>React</strong> 17.0
+				</li>
+				<li>
+					<strong>React Router</strong> 5.2
+				</li>
+				<li>
+					<strong>Firebase</strong> 9.1 as BaaS
+				</li>
+				<li>
+					<strike>Redux</strike> <strong>Redux-like</strong> React
+					Context API + useReducer{" "}
+					<strong>state management pattern</strong>
+				</li>
+				<li>
+					<strong>Bootstrap</strong> 5.1
+				</li>
+				<li>
+					<strong>CSS3</strong> (<strong>Flexbox & Grid</strong>{" "}
+					Layouts)
+				</li>
+				<li>
+					<strong>p5.js</strong> (for visualisations or animations)
+				</li>
+				<li>
+					<strong>Github & GitHub Pages</strong>
+				</li>
+				<li>
+					<strong>jQuery</strong>
+				</li>
+				<li>
+					<strong>Atomic Design</strong> methodology for interface
+					design
+				</li>
+			</ul>
+			<h3>What else I am interested in</h3>
+			<ul>
+				<li>visual thinking (sketchnoting and mindmapping)</li>
+				<li>composing music (using GuitarPro 7.5)</li>
+				<li>
+					creative process management (I wrote an ebook & ran blog on
+					the topic)
+				</li>
+				<li>abstract & logical board games & sudoku</li>
+			</ul>
+		</div>
+	);
 }
